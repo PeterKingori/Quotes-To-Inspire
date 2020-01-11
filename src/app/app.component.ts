8ccd8230cf54280c13b8a856cd284dd325851902
Peter Kingori <kpndegwa@gmail.com>
2020-01-11 23:36:06 +0300

    Create a quote component, import the quote class in quote.component.ts, move the quotes array from app.component.ts to quote.component.ts, and move the html and template logic from app.component.html to quote.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,5 @@ import { Quote } from './quote';
 })
 export class AppComponent {
   title = 'Quotes-To-Inspire';
-
-  quotes:Quote[] = [    
-    {id:1, text:'What we are is God\'s gift to us. What we become is our gift to God. - Eleanor Powell'},
-    {id:2, text:'Defeat is a state of mind. No one is ever defeated until defeat has been accepted as a reality. - Bruce Lee'},
-    {id:3, text:'An essential aspect of creativity is not being afraid to fail. - Edwin Land'},
-    {id:4, text:'Most of our troubles are due to our passionate desire for and attachment to things that we misapprehend as enduring entities. - Dalai Lama'},
-    {id:5, text:'A mind that is stretched by a new experience can never go back to its old dimensions. - Oliver Wendell Holmes, Jr.'}
-    ];
   }
   
