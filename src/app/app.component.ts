b9a9329d718f62059f16b94969cf2b0076b265d8
Peter Kingori <kpndegwa@gmail.com>
2020-01-11 22:49:20 +0300

    Create a Quote class, import it to app.component.ts and use it to create an array of quote objects of type Quote. To display each quote, the code in app.component.html in the list tag is changed to point to the quote text instead of the quote object.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Quote } from './quote';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Quotes-To-Inspire';
-  quotes: string[];
-  constructor() {
-    this.quotes = ['What we are is God\'s gift to us. What we become is our gift to God. - Eleanor Powell', 'Defeat is a state of mind. No one is ever defeated until defeat has been accepted as a reality. - Bruce Lee', 'An essential aspect of creativity is not being afraid to fail. - Edwin Land', 'Most of our troubles are due to our passionate desire for and attachment to things that we misapprehend as enduring entities. - Dalai Lama', 'A mind that is stretched by a new experience can never go back to its old dimensions. - Oliver Wendell Holmes, Jr.'];
+
+  quotes:Quote[] = [    
+    {id:1, text:'What we are is God\'s gift to us. What we become is our gift to God. - Eleanor Powell'},
+    {id:2, text:'Defeat is a state of mind. No one is ever defeated until defeat has been accepted as a reality. - Bruce Lee'},
+    {id:3, text:'An essential aspect of creativity is not being afraid to fail. - Edwin Land'},
+    {id:4, text:'Most of our troubles are due to our passionate desire for and attachment to things that we misapprehend as enduring entities. - Dalai Lama'},
+    {id:5, text:'A mind that is stretched by a new experience can never go back to its old dimensions. - Oliver Wendell Holmes, Jr.'}
+    ];
   }
-}
+  
